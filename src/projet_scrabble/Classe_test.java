7adb9afe64e5d9a3bc00b18091589f2d0204a8fd
Poncheele <clementponcheele@gmail.com>
2020-03-13 16:13:09 +0100

    modification de la fonction qui initialise le dico, elle fonctionne et elle est plutot rapide on va pouvoir d'en servir

--- src/projet_scrabble/Classe_test.java
+++ src/projet_scrabble/Classe_test.java
@@ -8,46 +8,39 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 public class Classe_test {
-	
-	public Classe_test() throws IOException {
 	HashMap<String, ArrayList<String>> h = new HashMap<String, ArrayList<String>>();
-	ArrayList<String> list = new ArrayList<String>();
-	
-	    BufferedReader lecteurAvecBuffer = null;
-	    String ligne;
 
-	    try
-	      {
-		lecteurAvecBuffer = new BufferedReader(new FileReader("dictionnaire.txt"));
-	      }
-	    catch(FileNotFoundException exc)
-	      {
-		System.out.println("Erreur d'ouverture");
-	      }
-	    while ((ligne = lecteurAvecBuffer.readLine()) != null)
-	    {
-	      list.add(ligne);
-	    }
-	    lecteurAvecBuffer.close();
-	    //System.out.println(list.get(50000));
-	  
-	  for (int i=0; i< list.size(); i++) {
-		  String cle = list.get(i).substring(0,1);
-		  if (h.containsKey(cle)) {
-			  ArrayList<String> liste = h.get(cle);
-			  liste.add(list.get(i));
-			  h.put(cle, liste);
-		  }
-		  else {
-			  h.put(cle, new ArrayList<String>());
-		  }
-	  }
-	    
-	  }
-	
-	
-public static void main(String[] argv) throws IOException
-{
-	Classe_test c = new Classe_test();
-}
+	public Classe_test() throws IOException {
+		this.h = new HashMap<String, ArrayList<String>>();
+		ArrayList<String> list = new ArrayList<String>();
+
+		BufferedReader lecteurAvecBuffer = null;
+		String ligne;
+
+		try {
+			lecteurAvecBuffer = new BufferedReader(new FileReader("dictionnaire.txt"));
+		} catch (FileNotFoundException exc) {
+			System.out.println("Erreur d'ouverture");
+		}
+		while ((ligne = lecteurAvecBuffer.readLine()) != null) {
+			String cle = ligne.substring(0, 2);
+			if (h.containsKey(cle)) {
+				ArrayList<String> liste = h.get(cle);
+				liste.add(ligne);
+				h.put(cle, liste);
+			} else {
+				ArrayList<String> l = new ArrayList<String>();
+				l.add(ligne);
+				h.put(cle,l);
+
+			}
+
+		}
+		lecteurAvecBuffer.close();
+
+	}
+
+	public static void main(String[] argv) throws IOException {
+		Classe_test c = new Classe_test();
+	}
 }
