212d17124b47fd738f835a4ee118b8abeddfeacb
romane27 <quintinromane2799@gmail.com>
2020-03-13 21:14:10 +0100

    page d'accueil permet d'afficher le plateau et les lettres

--- src/projet_scrabble/page_accueil.java
+++ src/projet_scrabble/page_accueil.java
@@ -1,6 +1,39 @@
 package projet_scrabble;
 
-public class page_accueil {
+import java.awt.BorderLayout;
+import java.awt.Dimension;
 
-}
+import javax.swing.JFrame;
+import javax.swing.JPanel;
 
+public class page_accueil extends JFrame{
+	public page_accueil() {
+		this.setSize(1000,1000);
+
+		//this.pack();
+		String [] lettre_joueurs = {"A","B","C","D","E","F","G"};
+		plateau p = new plateau (lettre_joueurs);
+		//JPanel panel = new JPanel();
+		//bouton bouton_lettres = new bouton(panel,lettre_joueurs);
+		//bouton bouton_lettre = new bouton(lettre_joueurs);
+		//this.add(bouton_lettres,BorderLayout.SOUTH);
+		//panel.setBounds(100, 50, 80, 80);
+		//panel.setPreferredSize(new Dimension(100,100));
+		//this.getContentPane().add(panel, BorderLayout.SOUTH);
+		//this.add(panel,BorderLayout.SOUTH);
+		
+		//this.add(le,BorderLayout.SOUTH);
+		this.add(p);
+		
+	//	this.pack();
+		this.setLocationRelativeTo(null); 
+		//this.addWindowListener(this);
+		this.setVisible(true);
+	}
+
+
+
+public static void main(String[] args) {
+	// TODO Auto-generated method stub
+	new page_accueil();
+}}
