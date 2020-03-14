37b529691d15807468b83d3c4655de8e3ea29935
Poncheele <clementponcheele@gmail.com>
2020-03-14 17:14:09 +0100

    modification de pupitre (mtn une arraylist et la pioche est mtn la meme que le jeu)

--- src/projet_scrabble/Pupitre.java
+++ src/projet_scrabble/Pupitre.java
@@ -1,17 +1,24 @@
 package projet_scrabble;
 
 import java.io.IOException;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Random;
 
 public class Pupitre {
+	ArrayList<String> pupitre;
+	
 	public Pupitre() {
-		Character[] pupitre = new Character[7];
-		Character[] pioche = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r',
-				's', 't', 'u', 'v', 'w', 'x', 'y', 'z' };
+		this.pupitre = new ArrayList<String>();
+		String [] lettres = {" "," ","E","E","E","E","E","E","E","E","E","E","E","E","E","E","E","A","A","A","A","A","A","A","A","A","I","I","I","I","I","I","I","I","N","N","N","N","N","N","O","O","O","O","O","O","R","R","R","R","R","R","S","S","S","S","S","S","T","T","T","T","T","T","U","U","U","U","U","U","L","L","L","L","L","D","D","D","M","M","M","G","G","B","B","C","C","P","P","F","F","H","H","V","V","J","Q","K","W","X","Y","Z"}; 
+		ArrayList<String> pioche = new ArrayList<String>(Arrays.asList(lettres));
 		Random r = new Random();
 		for (int i = 0; i < 7; i++) {
-			pupitre [i]=pioche[r.nextInt(25)];
-		}
+			int nb_random=r.nextInt(lettres.length);
+			pupitre.add(pioche.get(nb_random));
+			pioche.remove(nb_random);
+		}System.out.println(pupitre);
 	}public static void main(String[] argv) throws IOException {
 		Pupitre p = new Pupitre();
 	}
