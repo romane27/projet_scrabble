4482d1b525d9d7ffc31a370e6d3272bc84b2d1d9
poncheele clement <clementponcheele@gmail.com>
2020-04-06 20:36:32 +0200

    résolution des problèmes de surperposition des mots et de la maj du clavier

--- projet_scrabble-test2/src/Vue/Vue.java
+++ projet_scrabble-test2/src/Vue/Vue.java
@@ -15,6 +15,7 @@ import Modele.Joueur;
 import Modele.Tableau;
 
 import Vue2.duree_tours;
+
 public class Vue extends JFrame {
 	public Clavier clavier;
 	public Plateau plateau;
@@ -22,14 +23,14 @@ public class Vue extends JFrame {
 	public JButton melanger;
 	public Scores score;
 	public IHMChrono chrono;
-	
+
 	public Vue(Tableau tableau, Joueur joueur, Joueur joueur2) {
 		score = new Scores(4);
 		score.setBounds(670, 150, 150, 120);
 		score.setVisible(true);
 		plateau = new Plateau(tableau);
 		clavier = new Clavier(joueur);
-		//this.setLayout(null);
+		// this.setLayout(null);
 		plateau.setBounds(0, 0, 640, 640);
 		clavier.setBounds((640 - (640 / 15) * 7) / 2, 640 + 10, 640 / 15 * 7, 640 / 15);
 		fdt = new JButton("Fin de Tour");
@@ -39,11 +40,11 @@ public class Vue extends JFrame {
 		this.setSize(900, 850);
 		ImageIcon melange = new ImageIcon("src/images/melanger.png");
 		melanger = new JButton(melange);
-	
+
 		melanger.setBounds(550, 650, melange.getIconHeight(), melange.getIconWidth());
-		
+
 		this.add(melanger);
-		//this.setResizable(false);
+		// this.setResizable(false);
 		this.getContentPane().setBackground(Color.white);
 		this.add(fdt);
 		this.add(score);
@@ -56,10 +57,10 @@ public class Vue extends JFrame {
 	public void ajoutactlist(ActionListener e) {
 		fdt.addActionListener(e);
 	}
+
 	public void emplacement_lettre(ActionListener e) {
 		melanger.addActionListener(e);
 	}
-
 
 	public void majclavier(Joueur joueur) {
 		Clavier claviertemp = new Clavier(joueur);
@@ -68,18 +69,20 @@ public class Vue extends JFrame {
 			Bouton btn = (Bouton) claviertemp.getComponent(i);
 			Bouton btn2 = (Bouton) clavier.getComponent(i);
 			btn2.setIcon(btn.lettre.image);
-			//btn2.setText(btn.lettre.nom);
-			//btn2.setBackground(Color.white);
+			// btn2.setText(btn.lettre.nom);
+			// btn2.setBackground(Color.white);
 			btn2.lettre = btn.lettre;
+			btn2.verrouille = false;
 		}
 
 	}
-	public void melangeclavier (int [] emplacement,Joueur joueur) {
+
+	public void melangeclavier(int[] emplacement, Joueur joueur) {
 		Clavier claviertemp = new Clavier(joueur);
 		for (int i = 0; i < 7; i++) {
 			Bouton btn = (Bouton) claviertemp.getComponent(i);
 			Bouton btn2 = (Bouton) clavier.getComponent(emplacement[i]);
-			//btn2.place=emplacement[i];
+			// btn2.place=emplacement[i];
 			btn2.setIcon(btn.lettre.image);
 			btn2.lettre = btn.lettre;
 		}
@@ -89,15 +92,18 @@ public class Vue extends JFrame {
 		for (int i = 0; i < 7; i++) {
 			Bouton btn = (Bouton) clavier.getComponent(i);
 			btn.setIcon(btn.lettre.image);
-			//btn.setBackground(Color.white);
+			btn.verrouille = false;
+
+			// btn.setBackground(Color.white);
 		}
 	}
+
 	public void majplateau(int k, ImageIcon nom) {
 //		public void majplateau(int k, String nom) {
-			Bouton bouton = (Bouton) plateau.getComponent(k);
-			//bouton.setBackground(Color.white);
-			bouton.setIcon(nom);
-			//bouton.setText(nom);
-		}
+		Bouton bouton = (Bouton) plateau.getComponent(k);
+		// bouton.setBackground(Color.white);
+		bouton.setIcon(nom);
+		// bouton.setText(nom);
+	}
 
 }
